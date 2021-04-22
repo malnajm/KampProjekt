660b414025c378868dcba23b8c3f70e353f0efb5
duberg2 <hanna.duberg@gmail.com>
2021-04-22 14:59:27 +0200

    First feature

---
+++ src/test/java/stepDefinitions/StepDefintions.java
@@ -0,0 +1,44 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefintions {
+    @Given("I have entered {string}")
+    public void i_have_entered(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("I have also entered {string}")
+    public void i_have_also_entered(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I have typed {string}")
+    public void i_have_typed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I have written {string}")
+    public void i_have_written(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I have added {string}")
+    public void i_have_added(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I press enter")
+    public void i_press_enter() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("they should be displayed on the screen")
+    public void they_should_be_displayed_on_the_screen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/Features/Name.feature
+++ src/test/resources/Features/Name.feature
@@ -2,14 +2,14 @@ Feature: I need to be able to add and remove contestants so I can log their resu
 
   Scenario Outline: add a new contestant
 
-    Given    I have entered <name>
-    Given    I have entered <city>
-    Given    I have entered <country>
-    Given    I have entered <gender>
-    Given    I have entered <age>
+    Given    I have entered "<name>"
+    Given    I have also entered "<city>"
+    Given    I have typed "<country>"
+    Given    I have written "<gender>"
+    Given    I have added "<age>"
     When    I press enter
     Then    they should be displayed on the screen
 
     Examples:
       | name | city   | country | gender | age |
-      | Lisa | Örebro | Sweden  | Boy    | 22  |
+      | Lisa | Orebro | Sweden  | Boy    | 22  |
