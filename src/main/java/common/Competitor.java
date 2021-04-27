4829d752c25f83f794c671370862ea9e4afa5e66
duberg2 <hanna.duberg@gmail.com>
2021-04-27 13:22:29 +0200

    changed stuff

---
+++ src/main/java/common/Competitor.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.util.Scanner;
+
+public class Competitor {
+
+    Scanner sc = new Scanner(System.in);
+
+    String name = sc.nextLine();
+    System.out.println("Metod 1");
+
+
+}
--- src/test/java/stepDefinitions/StepDefintions.java
+++ src/test/java/stepDefinitions/StepDefintions.java
@@ -6,28 +6,28 @@ import io.cucumber.java.en.When;
 
 public class StepDefintions {
     @Given("I have entered {string}")
-    public void i_have_entered(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void i_have_entered(String name) {
+
+
     }
 
     @Given("I have also entered {string}")
-    public void i_have_also_entered(String string) {
+    public void i_have_also_entered(String city) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @Given("I have typed {string}")
-    public void i_have_typed(String string) {
+    public void i_have_typed(String country) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @Given("I have written {string}")
-    public void i_have_written(String string) {
+    public void i_have_written(String gender) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @Given("I have added {string}")
-    public void i_have_added(String string) {
+    public void i_have_added(String age) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
