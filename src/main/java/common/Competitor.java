3049d99870752aff96b3d9b1b9792b61cc36fc54
duberg2 <hanna.duberg@gmail.com>
2021-04-29 12:39:25 +0200

    added excel stuff

--- src/main/java/common/Competitor.java
+++ src/main/java/common/Competitor.java
@@ -3,7 +3,32 @@ package common;
 import java.util.Scanner;
 
 public class Competitor {
-/*
+
+    String namn;
+    int age;
+
+    public Competitor(String namn, int age) {
+        super();
+        this.namn = namn;
+        this.age = age;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    /*
 
 
     public String outPutName(String userInput) {
